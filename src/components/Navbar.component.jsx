a1dc108a83e6cbbcfa7667233171f7ef48334a5b
Muhammed Eldeeb <meldeeb1998@gmail.com>
2020-08-14 03:24:18 +0200

    desgin homepage navbar

--- src/components/Navbar.component.jsx
+++ src/components/Navbar.component.jsx
@@ -1,50 +1,47 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 import {logout} from './UserFunctions'
 
 const Navbar = () =>{
     return (
-        <div>
-            <span onClick={() => logout()}>Logout</span>
+    <header>
+        <div className="row">
+            <div className="col-md-3 text-left">
+                <Link to="/" className="logo">Logo</Link>
+            </div>
+            <div className="col-md-9 text-center">
+                <nav>
+                    <ul className="nav__links">
+                        <div className="row">
+                            <div className="col-md-8 text-left">
+                                <li>
+                                    <a className='nav-link' href='/recruiter/positions'>
+                                        Home
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className='nav-link' href='/recruiter/positions'>
+                                        {localStorage.getItem('first_name')}
+                                    </a>
+                                </li>
+                            </div>
+                            <div className="col-md-4 text-right">
+                                <li>
+                                    <a onClick={() => logout()} className='nav-link' href='/'>
+                                        logOut
+                                    </a>
+                                </li>
+                            </div>
+                        </div>
+
+
+                    </ul>
+                </nav>          
+            </div>
+            <div className="col-md-3 text-right"></div>
         </div>
-        // <nav className="navbar navbar-inverse " role="navigation">
-        //     <div className="container">
-        //         {/* <!-- Brand and toggle get grouped for better mobile display --> */}
-        //         <div className="navbar-header">
-        //             <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
-        //                 <span className="sr-only">Toggle navigation</span>
-        //                 <span className="icon-bar"></span>
-        //                 <span className="icon-bar"></span>
-        //                 <span className="icon-bar"></span>
-        //             </button>          
-        //         </div>
-        //         <a className="navbar-brand" href="/">Brand</a>
-        //         {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
-        //         <div className="collapse navbar-collapse" id="navbar-collapse-1">
-        //             <ul className="nav navbar-nav navbar-left">
-        //                 <li><a href="/">Menu</a></li>
-        //                 <li><a href="/">Menu</a></li>
-        //             </ul>
-        //             <ul className="nav navbar-nav navbar-right">
-        //                 <li><a href="/">Link</a></li>
-        //                 <li><a href="/">Link</a></li>
-        //                 <li><a href="/">Link</a></li>
-        //                 <li className="dropdown">
-        //                     <a href="/" className="dropdown-toggle" data-toggle="dropdown">Dropdown <b className="caret"></b></a>
-        //                     <ul className="dropdown-menu">
-        //                         <li><a href="/">Action</a></li>
-        //                         <li><a href="/">Another action</a></li>
-        //                         <li><a href="/">Something else here</a></li>
-        //                         <li className="divider"></li>
-        //                         <li><a href="/">Separated link</a></li>
-        //                     </ul>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //         {/* <!-- /.navbar-collapse --> */}
-        //     </div>
-        //     {/* <!-- /.navbar-container --> */}
-        // </nav>
+      
+    </header>
     )
 }
 
